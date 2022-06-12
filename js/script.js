ec269691e700aeaab8d0875cef455bb1950ecee7
josezhz <zhang.haozhe@trentglobal.edu.sg>
2022-06-12 08:04:34 +0000

    Added a baar chart in each popup showing scores of different indicators, and added country filter function

--- js/script.js
+++ js/script.js
@@ -23,6 +23,7 @@ let overlayMaps = {
 }
 let layerControl = L.control.layers(null, overlayMaps).addTo(map)
 
+// generate options for rank filter
 let selectRankS = document.querySelector('#select-rank-s')
 let selectRankE = document.querySelector('#select-rank-e')
 for (let i = 1; i <= 50; i++) {
@@ -40,6 +41,18 @@ for (let i = 1; i <= 50; i++) {
     selectRankE.appendChild(optionElement)
 }
 
+// generate options for country filter
+(async function run() {
+    let response = await axios.get('../json/countries_info.json')
+    let countryCodes = response.data.country_code[0]
+    for (eachCountry in countryCodes) {
+        let optionElementCountry = document.createElement('option')
+        optionElementCountry.innerHTML = eachCountry
+        optionElementCountry.value = countryCodes[eachCountry]
+        document.querySelector('#select-country').appendChild(optionElementCountry)
+    }
+})()
+
 document.querySelector('#btnSubject').addEventListener('click', async function () {
     for (eachLayer of allLayers) {
         eachLayer.clearLayers()
@@ -48,20 +61,24 @@ document.querySelector('#btnSubject').addEventListener('click', async function (
     let subject = document.querySelector('#select-subject').value
     let resRankings = await axios.get('../json/qs_2021_with_latlng.json')
     let rankings = resRankings.data[subject]
-    let rankStart = Number(document.querySelector('#select-rank-s').value)
-    let rankEnd = Number(document.querySelector('#select-rank-e').value)
-    if (rankStart > rankEnd) {alert(`
+    let rankS = Number(document.querySelector('#select-rank-s').value)
+    let rankE = Number(document.querySelector('#select-rank-e').value)
+    if (rankS > rankE) {
+        alert(`
         Invalid Input @ Rank
         (second number must be larger or equal to first number)
-    `)}
+    `)
+    }
+
+    let countrySelected = document.querySelector('#select-country').value
 
     let resCountriesInfo = await axios.get('../json/countries_info.json')
     let countryCodes = resCountriesInfo.data.country_code[0]
     let regions = resCountriesInfo.data.region[0]
 
-    document.querySelector('#container-search-by-uni').style.zIndex = 701
+    document.querySelector('#container-search-by-uni').style.zIndex = 699
     document.querySelector('#unis').innerHTML = ''
-    for (let eachUni = rankStart - 1; eachUni < rankEnd; eachUni++) {
+    for (let eachUni = rankS - 1; eachUni < rankE; eachUni++) {
         let name = rankings[eachUni].Institution
         let country = rankings[eachUni].Location
         let rank = rankings[eachUni][2021]
@@ -114,49 +131,81 @@ document.querySelector('#btnSubject').addEventListener('click', async function (
 
         // generate popup
         let imgElementFlag = `<img src="https://countryflagsapi.com/png/${countryCode}" class="border rounded" height="30px"/>`
+        let imgElementSubject = `<img src="../images/subject_logos/${subject}.png" height="30px">`
+        let subjectName = {
+            overall: "OVERALL",
+            arts_and_humanities: "ARTS & HUMANITIES",
+            engineering_and_technology: "ENGINEERING & TECHNOLOGY",
+            life_sciences_and_medicine: "LIFE SCIENCES & MEDICINE",
+            natural_sciences: "NATURAL & SCIENCES",
+            social_sciences_and_management: "SOCIAL SCIENCES & MANAGEMENT"
+        }
         marker.bindPopup(`
+            <div class="d-flex">
+                ${imgElementSubject}
+                <span class="text-success fw-bold align-self-end ms-1">${subjectName[subject]}</span>
+            </div>
             <div class="d-flex justify-content-between">
                 <div class="align-self-end fs-5">Rank: <span class="fs-3">${rank}</span></div>
                 ${imgElementFlag}
             </div>
-            <hr class="m-0 mb-1">
+            <hr class="m-0 mb-1 p-1 bg-success rounded-pill">
             <h4>${name}</h4>
             <div id="popup-chart-${rank}" style="width: 300px;"></div>
         `).openPopup()
 
         // generate popup chart
+        let overallScore = rankings[eachUni].Score
+        let academicScore = rankings[eachUni].Academic
+        let employerScore = rankings[eachUni].Employer
+        let citationsScore = rankings[eachUni].Citations
+        let hScore = rankings[eachUni].H
         marker.addEventListener('click', function () {
             var options = {
-                chart: {
-                    type: 'line'
-                },
                 series: [{
-                    name: 'sales',
-                    data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+                    name: 'Socre',
+                    data: [overallScore, academicScore, employerScore, citationsScore, hScore]
                 }],
-                xaxis: {
-                    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-                }
-            }
+                chart: {
+                    type: 'bar',
+                    height: 200
+                },
+                plotOptions: {
+                    bar: {
+                        borderRadius: 4,
+                        horizontal: true,
+                    }
+                },
+                colors: ['#198754'],
+                dataLabels: { enabled: true },
+                xaxis: { categories: ['Overall', 'Academic', 'Employer', 'Citations', 'H-Index'] }
+            };
 
             var chart = new ApexCharts(document.querySelector(`#popup-chart-${rank}`), options);
-
             chart.render()
         })
 
-
-
         let region = regions[country]
-        if (region == "asia") {
-            marker.addTo(asiaLayer)
-        } else if (region == "europe") {
-            marker.addTo(europeLayer)
-        } else if (region == "latin_america") {
-            marker.addTo(latin_americaLayer)
-        } else if (region == "north_america") {
-            marker.addTo(north_americaLayer)
-        } else if (region == "oceania") {
-            marker.addTo(oceaniaLayer)
+        let selected = true
+        if (countrySelected) {
+            selected = false
+            if (countryCode == countrySelected) {
+                selected = true
+            }
         }
+        if (selected) {
+            if (region == "asia") {
+                marker.addTo(asiaLayer)
+            } else if (region == "europe") {
+                marker.addTo(europeLayer)
+            } else if (region == "latin_america") {
+                marker.addTo(latin_americaLayer)
+            } else if (region == "north_america") {
+                marker.addTo(north_americaLayer)
+            } else if (region == "oceania") {
+                marker.addTo(oceaniaLayer)
+            }
+        }
+
     }
 })
